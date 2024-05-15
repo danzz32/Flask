28a5525de561117528e93e9e31870227198b5ae7
DaN <danilosm721@gmail.com>
2024-05-15 15:07:32 -0300

    Inlcusão de html

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/contatos')
+def contato():
+    return 'Qualquer dúvida consulte um dos contatos abaixo'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
